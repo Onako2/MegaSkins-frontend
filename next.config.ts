8835c34f101c2c81ff8195defc0c10e5c037a6f2
Onako2 <79749977+Onako2@users.noreply.github.com>
2026-06-07 17:52:53 +0200

    add base path

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'standalone'
+  output: 'standalone',
+  basePath: "/megaskins"
 };
 
 export default nextConfig;
