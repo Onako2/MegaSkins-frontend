285d350649e5a5b00ee89ca9d09cc9568c004570
Onako2 <79749977+Onako2@users.noreply.github.com>
2026-06-08 17:42:55 +0200

    a small test

--- app/ui/stats.tsx
+++ app/ui/stats.tsx
@@ -9,7 +9,13 @@ export default async function Stats() {
     api = 'https://nuc.de.majic.rs/api/megaskins/stats'
   }
   try {
-    const res = await fetch(api)
+    const res = await fetch(api).catch(ex => {
+      return (
+      <div className="stats basis-1/2">
+        <p>{`Error fetching stats: ${String(ex)}`}</p>
+      </div>
+    )
+    })
     if (!res.ok) {
       const text = await res.json().catch(() => '')
       return (
