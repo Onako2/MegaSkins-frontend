2fd396d6eb3ec8c4243f550ad4a2b0616b564a5f
Onako2 <79749977+Onako2@users.noreply.github.com>
2026-08-03 17:17:28 +0200

    Less scrolling yay

--- app/search/[query]/page.tsx
+++ app/search/[query]/page.tsx
@@ -21,7 +21,7 @@ export default async function Page({ params }: { params: { query: string } } | {
   const { query } = await params as { query: string };
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full flex-col items-center py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex flex-1 w-full flex-col items-center py-8 px-16 bg-white dark:bg-black sm:items-start">
         <Link href="/">
           <h1 className="logo"><span>Mega<br></br>Skins</span></h1>
         </Link>
