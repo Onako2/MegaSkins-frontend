c706d17e786e42f182ed9cee8ee8e3d8dc3cdec6
Onako2 <79749977+Onako2@users.noreply.github.com>
2026-08-04 15:43:14 +0200

    add error handling

--- app/ui/check_safety.tsx
+++ app/ui/check_safety.tsx
@@ -13,18 +13,23 @@ export default function SafetyCheck({ hash }: SafetyProps) {
   return (
     <div className="basis-128 m-8 rounded-md grid">
       <p>{similarity}</p>
-      <button id="safety_button" className="outline-indigo-500 outline-dotted outline-3 outline-offset-5 hover:outline-yellow-500 transition-all duration-700" onClick={() => {
+      <button id="safety_button" className="mt-5 outline-indigo-500 outline-dotted outline-3 outline-offset-5 hover:outline-yellow-500 transition-all duration-700" onClick={() => {
         setSimilarity("Getting data..");
+        const safety_button = document.getElementById("safety_button");
         fetch(api).then(r => {
+          if (r.ok) {
             r.text().then(text => {
-                const safety_button = document.getElementById("safety_button");
-                safety_button?.remove()
-                setSimilarity("Safety score: " + text);
-              }
-            )
+              safety_button?.remove()
+              setSimilarity("Safety score: " + text);
+            })
+          } else {
+            safety_button?.classList.add("outline-red-500")
+            setSimilarity("⚠️ Error: " + r.status);
           }
+        }
         ).catch(e => {
-          setSimilarity(e);
+          safety_button?.classList.add("outline-red-500")
+          setSimilarity("⚠️ Error: " + e);
         })
       }}>Check MegaSkins Skin Safety</button>
       <p className="text-xs mt-5">The <span className="font-bold">MegaSkins Skin Safety</span> score tells you on a scale between 0.0 to 1.0 how unsafe a Minecraft skin is for use on Minecraft servers with minors. A score of 0.0 means that a skin is completly safe, and a skin of 1.0 means that a skin is an exact match. A score of 0.5 means that the skin is 50% similar to an unsafe skin.</p>
